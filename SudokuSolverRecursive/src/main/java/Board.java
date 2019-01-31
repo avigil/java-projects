f55d52c496c4119a33e258feaf370cdce8d2d889
avigi <avigi@avigil-lenovo-flex5>
2019-01-31 08:17:16 -0800

    Changed some methods to use java streams instead of for loops to improve performance. However, the traditional way is still there. Also, changed some Sets to int arrays because of Stack Overflow Exceptions. However, those likely won't occur with puzzles that are more complete.

--- SudokuSolverRecursive/src/main/java/Board.java
+++ SudokuSolverRecursive/src/main/java/Board.java
@@ -1,8 +1,11 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 class Board {
     private final List<List<Integer>> board;
@@ -11,6 +14,10 @@ class Board {
     	BoardReader br = BoardReaderFactory.getBoardReader(filePath);
     	board = br.readBoard(filePath);
     	
+    }
+    
+    Board(List<List<Integer>> boardList) {
+    	board = boardList;
     }
 
     List<List<Integer>> getBoard() {
@@ -38,10 +45,19 @@ class Board {
     }
     
     boolean isValid(){
-    	return numbersValid() && rowCheck() && columnCheck() && gridCheck();
+    	boolean nValid = numbersValid();
+    	boolean rValid = rowCheck();
+    	boolean cValid = columnCheck();
+    	boolean gValid = gridCheck();
+    	
+    	return nValid && rValid && cValid && gValid;
+    	
+    	//return numbersValid() && rowCheck() && columnCheck() && gridCheck();
     }
     
     private boolean numbersValid() {
+    	return board.parallelStream().flatMap(List::stream).allMatch(x -> x==null || (x > 0 && x < 10) );
+    	/*
     	for (int row = 0; row < board.size(); row++) {
     		List<Integer> rowList = board.get(row);
     		for (int column = 0; column < rowList.size(); column++) {
@@ -53,62 +69,102 @@ class Board {
     		}
     	}
     	return true;
+    	*/
     }
     
     private boolean rowCheck() {
+    	return !(board.parallelStream().map(row -> {
+    		Set<Integer> duplicatedNumbersRemovedSet = new HashSet<>();
+    		Optional<Integer> duplicate = row.parallelStream().filter(n->n!=null).filter(n -> !duplicatedNumbersRemovedSet.add(n)).findAny();
+    		return duplicate.isPresent();
+    		//row.parallelStream().filter(i -> Collections.frequency(row, i) >1).collect(Collectors.toSet()) )
+    	}).filter(b -> b).findAny().isPresent()
+    			);
+    	
+    	//flatMap(List::stream).allMatch(x -> x==null || (x > 0 && x < 10) );
+    	
+    	
+    	/*
     	for (int row = 0; row < board.size(); row++) {
     		List<Integer> rowList = board.get(row);
-    		Set<Integer> rowSet = new HashSet<>();
+    		//Set<Integer> rowSet = new HashSet<>();
+    		boolean [] rowArray = new boolean[9];
     		for (int column = 0; column < rowList.size(); column++) {
     			Integer currentValue = rowList.get(column);
     			if (currentValue != null) {
+    				if (rowArray[currentValue-1]) {
+    					return false;
+    				} else {
+    					rowArray[currentValue-1]=true;
+    				}
+    			} 
+    			
+    			/*if (currentValue != null) {
     				if (rowSet.contains(currentValue)) {
     					return false;
     				} else {
     					rowSet.add(currentValue);
     				}
-    			} 
-    		}
+    			} */
+    //		}
 
-    	}
-		return true;
+    //	}
+	//	return true;
     }
     
     private boolean columnCheck() {
     	for (int col = 0; col < board.size(); col++) {
-    		Set<Integer> colSet = new HashSet<>();
+    		//Set<Integer> colSet = new HashSet<>();
+    		boolean [] colArray = new boolean[9];
     		for (int row = 0; row < board.get(0).size(); row++) {
     			Integer currentValue = board.get(row).get(col);
+    			//int currentValueInt = board.get(row).get(col);
     			if (currentValue != null) {
-    				if (colSet.contains(currentValue)) {
+    				/*if (colSet.contains(currentValue)) {
     					return false;
     				} else {
     					colSet.add(currentValue);
+    				}*/
+    				
+    				if (colArray[currentValue-1]) {
+    					return false;
+    				} else {
+    					colArray[currentValue-1]=true;
     				}
     			} 
     		}
 
     	}
 		return true;
+		
     }
     
     private boolean gridCheck() {
     	for (int rowOffset = 0; rowOffset < board.size(); rowOffset+=3) {
     		for (int colOffset = 0; colOffset < board.size(); colOffset+=3) {
-    		Set<Integer> squareSet = new HashSet<>();
+    		//Set<Integer> squareSet = new HashSet<>();
+    		boolean [] squareArray = new boolean[9];
     		for (int row = 0; row < 3; row++) {
     			for (int col = 0; col < 3; col++) {
     				Integer currentValue = board.get(rowOffset + row).get(colOffset + col);
     				if (currentValue != null) {
-    					if (squareSet.contains(currentValue)) {
+    					if (squareArray[currentValue-1]) {
+    						return false;
+    					} else {
+    						squareArray[currentValue-1]=true;
+    					}
+    					
+    					
+    					/*if (squareSet.contains(currentValue)) {
     						return false;
     					} else {
     						squareSet.add(currentValue);
-    					}
+    					}*/
     				}
     		
     			}
     		}
+    		squareArray = null;
 
     		}
 
@@ -118,7 +174,47 @@ class Board {
     
     public ArrayList<Board> getNeighbors() {
     	
+    	//board.parallelStream().flatMap(List::stream).allMatch(x -> x==null || (x > 0 && x < 10) );
+    	
+    	
+    	
+    	
+    	ArrayList<Board> neighbors = new ArrayList<Board>();
+    	//go through the entire board and for each null spot, try every possible number
+    	for(int row=0;row<board.size();row++) {
+    		for(int column =0;column<board.get(row).size();column++) {
+    			List<Integer> rowValues = board.get(row);
+    			if(rowValues.get(column) ==null) {
+	    			for(int value = 1; value<10;value++) {
+	    				List<List<Integer>> duplicate = duplicateBoard(board);
+	    				List<Integer> changingRowValues = duplicate.get(row);
+	    				changingRowValues.set(column, value);
+	    				duplicate.set(row, changingRowValues);
+	    				//need to check if the created board is valid
+	    				Board boardToCheck = new Board(duplicate);
+	    				if(boardToCheck.isValid()) {
+	    					neighbors.add(boardToCheck);
+	    				}
+    				}
+    			}
+    		
+    		}
+    	}
+    	return neighbors;
     }
+    
+    private List<List<Integer>> duplicateBoard(List<List<Integer>> boardToDuplicate){
+    	List<List<Integer>> copy = new ArrayList<List<Integer>>();
+    	for(int r=0;r<boardToDuplicate.size();r++) {
+    		List<Integer> row = new ArrayList<Integer>();
+    		for(int c=0;c<boardToDuplicate.get(0).size();c++) {
+    			row.add(boardToDuplicate.get(r).get(c));
+    		}
+    		copy.add(row);
+    	}
+    	return copy;
+    }
+
     
     
     boolean isSolved () {
@@ -126,6 +222,9 @@ class Board {
     	if (! isValid) {
     		return false;
     	} else {
+    		boolean anyNulls = board.parallelStream().flatMap(List::stream).anyMatch(x -> x==null);
+    		return !anyNulls;
+    		/*
     		for (int row = 0; row < board.size(); row++) {
     			List<Integer> rowList = board.get(row);
     			for (int column = 0; column < rowList.size(); column++) {
@@ -135,9 +234,14 @@ class Board {
     				} 
     			} 
     		}
-    		return true;
+    		return true;*/
     	}
     	    	
     }
     
+    /*
+    public static Board clone(Board original) {
+    	original.getBoard().parallelStream().
+    }
+    */
 }
--- SudokuSolverRecursive/src/main/java/SudokuSolver.java
+++ SudokuSolverRecursive/src/main/java/SudokuSolver.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 public class SudokuSolver {
     private Board board;
@@ -10,13 +12,63 @@ public class SudokuSolver {
     public static void main(String [] args) throws IOException {
         SudokuSolver solver = new SudokuSolver("src/test/resources/puzzle.ss");
         System.out.println(solver.board);
+        
+        SudokuSolver solver2 = new SudokuSolver("src/test/resources/puzzleSolvedCompletePuzzle_3_missing.ss");
+        System.out.println(solver2.board);
+        
+        System.out.println("solved 2 board:\n" + solver2.solve());
     }
     
     public Board solve() {
-    	
+    	Board solvedBoard = solveHelper(board);
+    	return solvedBoard;
     }
     
-    public Board solveHelper () {
+    public Board solveHelper (Board boardToSend) {
+    	//base case
+    	if(boardToSend.isSolved()) {
+    		return boardToSend;
+    	}
     	
+    	Board returnBoard=null;
+    	
+    	//otherwise we need to check all of the neighbors
+    	List<Board> neighbors = boardToSend.getNeighbors();
+    	/*
+    	Optional<Board> solvedNeighbor = neighbors.parallelStream().filter(b -> b.isSolved()).findAny();
+    	if(solvedNeighbor.isPresent()) {
+    		return solvedNeighbor.get();
+    	}
+    	*/
+    	Optional<Board> solvedRecursiveBoard = neighbors.parallelStream().map(board -> {
+    			return solveHelper(board);	
+    	}).filter(b -> b!=null && b.isSolved()).findAny();
+    	//filter(b -> solveHelper(b)!=null && solveHelper(b).isSolved())
+    	
+    	if(solvedRecursiveBoard.isPresent()) {
+    		return solvedRecursiveBoard.get();
+    	}
+    	
+    	/*
+    	for(Board b: neighbors) {
+    		if(b.isSolved()) {
+    			return b;
+    		} else if(b.isValid()){
+    			Board recursiveBoard = solveHelper(b);
+    			if(recursiveBoard!=null && recursiveBoard.isSolved()) {
+    				return recursiveBoard;
+    			}
+    			//System.out.println(recursiveBoard);
+    		}
+    		/*
+    		try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+    	}*/
+    	return returnBoard;
     }
 }
